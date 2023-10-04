22f895448a5b9a0242ef490eec839a39d2a06bb2
July <julioastudillo3150@gmail.com>
2023-10-04 17:31:59 -0400

    contact challenge v2

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  
+  theme: {
+    fontSize: {
+      'xl': '64px',
+      'lg': '32px',
+      'base': '16px',
+      'sm': '14px',
+      'button': '20px',
+    },
+    fontFamily: {
+      'vietnam' : ["Be Vietnam Pro", 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
